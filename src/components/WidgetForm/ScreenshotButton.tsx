868f8854bc4204f8d34fab9c54e4dda044414a10
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-05-07 16:39:36 -0300

    chore: set up deploy

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -31,8 +31,6 @@ export function ScreenshotButton({ onScreenshotTook, screenshot }: ScreenshotBut
         onClick={() => onScreenshotTook(null)}
         style={{
           backgroundImage: `url(${screenshot})`,
-          backgroundPosition: 'right bottom',
-          backgroundSize: 180,
         }}
       >
         <Trash weight="fill"/>
